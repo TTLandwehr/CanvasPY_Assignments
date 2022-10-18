6d51199796f8deae6c73d9cae690cb3528169b49
TTLandwehr <tobylandwehr1776@gmail.com>
2022-10-18 18:19:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,51 +6,62 @@ import time
 startTime=time.time()
 #CANVAS API URL
 API_URL="https://canvas.unf.edu"
-
 #CANVAS API key
-#Hidden for privacy
-API_KEY=None
+API_KEY="4502~fEGTycFhFVt7AJab3A0PhUnfo0L7cLv3W1uYWPbeb4LSlYqPe2tlyNoMdKGMRwb7"
 # TODO:
 # have program read from setup file 
 # create function that prints all assignments
-
+# create function that gets and stores API key of user
 #New canvas object
 canvas=Canvas(API_URL,API_KEY)
 #SLL for holding assignments
-AList=sllist()
+CList=sllist()
 user=canvas.get_current_user()
-print("The current user is:")
-print(user)
-print("------------------------------")
-print("Currently enrolled classes are")
-print("------------------------------")
-course=user.get_enrollments()
 #load courses in SLL and print current courses
+print("Please wait while we load your classes")
 for Course in user.get_enrollments():
         active_courses=canvas.get_course(Course.course_id)
-        AList.append(active_courses)
-        print(active_courses)
-print("--------------------------------")
+        CList.append(active_courses)
+print("The current user is:")
+print(canvas.get_current_user())
+print("------------------------------")        
 print("Enter number to show assignments for class")
 print("Enter A to show assignemnts for all classes")
 print("---------------------------------")
-for i in range(AList.size):
-        print(AList[i],end=" #")
+for i in range(CList.size):
+        print(CList[i],end=" #")
         print(i)
 assignID=input("Enter number next to class\n")
 if assignID.isnumeric():
         try:
-                temp=AList[int(assignID)].get_assignments()
+                temp=CList[int(assignID)].get_assignments()
                 temp[0]
         except:
                 print("There are no assignements for this class")
-
-        
+                exit
         for assignment in temp:
-         print(assignment)
+                tempassignvar=assignment
+                print(tempassignvar, end="---")
+                print("Due at",end=": ")
+                print(tempassignvar.due_at)
 else:
-        print("TODO: Implmemnt all assignments")
+        for i in range(CList.size):
+                temp=CList[i].get_assignments()
+                print("Assignments in class:")
+                print(CList[i])
+                print("---------------------")
+                for assignment in temp:
+                        tempassignvar=assignment
+                        print(tempassignvar, end="---")
+                        print("Due at",end=": ")
+                        print(tempassignvar.due_at)
 exitTime=(time.time()-startTime)
 print(exitTime)
+
+
+
+
+
+
 
 
