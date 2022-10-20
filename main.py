81d7f44b92dc1ffe7f0b6e340e62c93c42758aad
TTLandwehr <tobylandwehr1776@gmail.com>
2022-10-20 13:07:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ startTime=time.time()
 #CANVAS API URL
 API_URL="https://canvas.unf.edu"
 #CANVAS API key
-#API_KEY="4502~fEGTycFhFVt7AJab3A0PhUnfo0L7cLv3W1uYWPbeb4LSlYqPe2tlyNoMdKGMRwb7"
 API_KEY=create_file()
 # TODO: 
 # create function that gets and stores API key of user
